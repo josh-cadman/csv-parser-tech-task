5852d3e80c7eb508dd9ea7cc3eb17b53919ce6cd
Joshua Cadman <josh_cadman@icloud.com>
2025-09-03 18:57:20 +0100

    Creating a post route that will parse through the CSV and return the results as a prop

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\HomeownerParserInterface;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class UploadController extends Controller
 {
+    public function __construct(protected HomeownerParserInterface $homeownerInterface)
+    {}
+
     public function index(): Response
     {
         return Inertia::render('index');
     }
+
+    public function store(Request $request): Response
+    {
+        $request->validate([
+            'file' => 'required|file|mimes:csv'
+        ]);
+
+        $parseResults = $this->homeownerInterface->parseCsvFile($request->file('file'));
+
+        return Inertia::render('index', [
+            'output' => $parseResults
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UploadController::class, 'index']);
+Route::post('/', [UploadController::class, 'store']);
