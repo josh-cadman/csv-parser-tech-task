e413481c8f076bdb984065c765ed1e1b90cca5ca
Joshua Cadman <josh_cadman@icloud.com>
2025-09-03 19:22:46 +0100

    Adding meta title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Homeowner Parser | Street Group</title>
         @vite('resources/js/app.js')
         @vite('resources/css/app.css')
         @inertiaHead
